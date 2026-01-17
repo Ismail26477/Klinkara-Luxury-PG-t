6ec8f0c358a9327ebfac446a5300bded9cf32f92
Ismail Sheikh <129026460+Ismail26477@users.noreply.github.com>
2026-01-17 12:54:21 +0530

    Update NearbyServices.tsx

--- src/components/NearbyServices.tsx
+++ src/components/NearbyServices.tsx
@@ -49,7 +49,7 @@ const NearbyServices = () => {
         </motion.div>
 
         {/* Cards */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {nearbyPlaces.map((place, index) => (
             <motion.div
               key={place.title}
