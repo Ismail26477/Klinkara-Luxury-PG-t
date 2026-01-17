f51b45f49d8c38c7ab5790d4c690431572633684
Ismail Sheikh <129026460+Ismail26477@users.noreply.github.com>
2026-01-17 12:54:33 +0530

    Update Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -120,7 +120,7 @@ const Gallery = () => {
         {/* Gallery Grid */}
         <motion.div
           layout
-          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
+          className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
         >
           <AnimatePresence mode="popLayout">
             {filteredImages.map((image, index) => (
